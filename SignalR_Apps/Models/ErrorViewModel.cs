505f281dd2dcf610fc508fdb04f2bd5e2e2ce0bd
lazuardi_fenjano <lazuardifenzan@gmail.com>
2022-01-17 16:44:16 +0700

    SignalR ,message to Specific User

---
+++ SignalR_Apps/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SignalR_Apps.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
